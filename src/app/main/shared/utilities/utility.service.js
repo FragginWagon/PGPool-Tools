c7163d0cae6c9b2cb23616169301affeb63fa739
Greg Jacobs <greg.jacobs@binarywasteland.com>
2018-03-09 13:38:25 -0500

    create stock utility service
    can be enhanced later on to hold functions used throughout the application

---
+++ src/app/main/shared/utilities/utility.service.js
@@ -0,0 +1,20 @@
+/**
+ * Created by FragginWagon on 2018-03-09.
+ */
+(function () {
+    'use strict';
+    
+    angular
+    .module('app.utilities')
+    .service('UtilityService', UtilityService);
+    
+    /** @ngInject **/
+    function UtilityService() {
+        
+        var service = {
+        
+        };
+        
+        return service;
+    }
+})();
